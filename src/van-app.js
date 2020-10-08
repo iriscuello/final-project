37e3ef06aaf5080be4771106f70e09acf3868489
iriscuello <64563326+iriscuello@users.noreply.github.com>
2020-10-07 20:33:43 -0400

    moved around links in head of html

---
+++ src/van-app.js
@@ -0,0 +1,6 @@
+let key = "f6daddd2490e280fc02eb01a9840f82a";
+let api =
+  "https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=f6daddd2490e280fc02eb01a9840f82a";
+
+console.log(api);
+console.log(key);
