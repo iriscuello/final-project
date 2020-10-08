833b04b3928d35b7b9625cde76595ba30d9a44bd
iriscuello <64563326+iriscuello@users.noreply.github.com>
2020-10-07 20:45:42 -0400

    added variables for api call for weather

--- src/van-app.js
+++ src/van-app.js
@@ -1,5 +1,10 @@
+
+
+
 let key = "f6daddd2490e280fc02eb01a9840f82a";
-let api =
-  "https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=f6daddd2490e280fc02eb01a9840f82a";
+let city = "New York";
+let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
 
 console.log(api);
+
+axios.get(api).then(displayTemp);
