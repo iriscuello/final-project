83b5b9abe2257528756837a0d63702bfab953bcc
iriscuello <64563326+iriscuello@users.noreply.github.com>
2020-10-18 16:56:56 -0400

    started over in JS
    Getting confused so started again with my code for the temp. Figured out and adding all pertinent weather data.

--- src/van-app.js
+++ src/van-app.js
@@ -1,23 +1,28 @@
-function displayTemp(response) {
+//function displayTemp(event)
+let key = "f6daddd2490e280fc02eb01a9840f82a";
+let city = "Toronto";
+let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
-   let temp = Math.round(response.data.main.temp);
-  let faren = (temp * 9) / 5 + 32;
-  let farTemp = document.querySelector("#temperature");
-  farTemp.innerHTML = faren;
+function tempToFar(response) {
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  console.log(response.data);
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(response.data.main.temp);
+  let realFeel = document.querySelector("#realFeel");
+  realFeel.innerHTML =
+    "Feels Like: " + Math.round(response.data.main.feels_like) + "℃";
+  //   let temp = Math.round(response.data.main.temp);
+  //   let faren = (temp * 9) / 5 + 32;
+  //   let farTemp = document.querySelector("#temperature");
+  //   farTemp.innerHTML = faren;
 }
+axios.get(api).then(tempToFar);
+// function currentTemp() {
 
-function tempToFar(event) {
-  
-  let key = "f6daddd2490e280fc02eb01a9840f82a";
-  let city = "Toronto";
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  
-  axios.get(api).then(getCurrentTemp);
-}
-let fLink = document.querySelector("#farenheight");
-fLink.addEventListener("click", tempToFar);
+// }
+// let fLink = document.querySelector("#farenheight");
+// fLink.addEventListener("click", displayTemp);
 
 let now = new Date();
 let hour = now.getHours();
