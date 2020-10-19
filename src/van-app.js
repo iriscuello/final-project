5477b0b0ecab56e27f1a8d2998ce2d7823ef8897
iriscuello <64563326+iriscuello@users.noreply.github.com>
2020-10-19 16:46:47 -0400

    auto complete off/JS add functions
    Created functions for search engine  and moved things around a bit

--- src/van-app.js
+++ src/van-app.js
@@ -1,51 +1,3 @@
-function tempToFar(response) {
-  console.log(response.data);
-  let currentCity = document.querySelector("#city");
-  currentCity.innerHTML = response.data.name;
-  let description = document.querySelector("#description");
-  description.innerHTML = response.data.weather[0].description;
-  let tempElement = document.querySelector("#temperature");
-  tempElement.innerHTML = Math.round(response.data.main.temp);
-  let realFeel = document.querySelector("#realFeel");
-  realFeel.innerHTML =
-    "Feels Like: " + Math.round(response.data.main.feels_like) + "℃";
-  let high = document.querySelector("#hiTemp");
-  high.innerHTML = "H " + Math.round(response.data.main.temp_max) + "℃";
-  let span = document.querySelector("#lowTemp");
-  span.innerHTML = "L " + Math.round(response.data.main.temp_min) + "℃";
-  let windSpeed = document.querySelector("#windSpeed");
-  windSpeed.innerHTML =
-    "Wind Speed: " + Math.round(response.data.wind.speed) + " km/h";
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML =
-    "Humidity: " + Math.round(response.data.main.humidity) + " %";
-  let icon = document.querySelector("#icon");
-  icon.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-
-  icon.setAttribute("alt", response.data.weather[0].description);
-  //   let temp = Math.round(response.data.main.temp);
-  //   let faren = (temp * 9) / 5 + 32;
-  //   let farTemp = document.querySelector("#temperature");
-  //   farTemp.innerHTML = faren;
-}
-
-function searchCity(search) {
-  let key = "f6daddd2490e280fc02eb01a9840f82a";
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  axios.get(api).then(tempToFar);
-}
-
-function returnInputData(event) {
-  event.preventDefault();
-  let dataReturn = document.querySelector("#searchEngine");
-  search(dataReturn.value);
-}
-let searchSubmit = document.querySelector("#search-form");
-searchSubmit.addEventListener("click", returnInputData);
-
 let now = new Date();
 let hour = now.getHours();
 let twelve = hour % 12 || 12;
@@ -83,3 +35,47 @@ let day = days[now.getDay()];
 
 let dateDisplayed = document.querySelector("#dateTime");
 dateDisplayed.innerHTML = `${day} ${month} ${date} ${year} ${twelve}:${min}`;
+
+function tempDisplay(response) {
+  let currentCity = document.querySelector("#city");
+  currentCity.innerHTML = response.data.name;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let realFeel = document.querySelector("#realFeel");
+  realFeel.innerHTML =
+    "Feels Like: " + Math.round(response.data.main.feels_like) + "℃";
+  let high = document.querySelector("#hiTemp");
+  high.innerHTML = "H " + Math.round(response.data.main.temp_max) + "℃";
+  let span = document.querySelector("#lowTemp");
+  span.innerHTML = "L " + Math.round(response.data.main.temp_min) + "℃";
+  let windSpeed = document.querySelector("#windSpeed");
+  windSpeed.innerHTML =
+    "Wind Speed: " + Math.round(response.data.wind.speed) + " km/h";
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML =
+    "Humidity: " + Math.round(response.data.main.humidity) + " %";
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
+}
+
+function searchCity(city) {
+  let key = "f6daddd2490e280fc02eb01a9840f82a";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(api).then(tempDisplay);
+}
+
+function returnInputData(event) {
+  event.preventDefault();
+  let dataReturn = document.querySelector("#searchEngine");
+  searchCity(dataReturn.value);
+}
+searchCity("Toronto");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", returnInputData);
